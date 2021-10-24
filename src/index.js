e4ad1f8bc2428ea6dfab1156dd0e4630739481bb
Libin Varghese [PC-BLR-DEV] <libin@pointcross.net>
2021-10-24 13:38:21 +0530

    ReactBasics project first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Greetings() {
+  return (
+    //React Fragment <> or <React Fragment>
+    //An alternative for using JSX is by calling React.createElement('h2',{props},'Hello')
+    <>
+      <div>
+        <h2>Hello World</h2>
+        <a href="#">Hello World</a>
+      </div>
+      <h4>This is my first component </h4>
+    </>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<Greetings />, document.getElementById("root"));
